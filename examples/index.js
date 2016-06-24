cdbf8c2678c05b09dca8cd76886dfe95c51d2a9d
bloodyKnuckles <treenopie@gmail.com>
2016-06-23 20:37:21 -0500

    Cleanup.

--- examples/index.js
+++ examples/index.js
@@ -1,11 +1,10 @@
-var shaved = require('../')
+var st = require('../')
 var fs = require('fs')
-var sd = require('snabbdom')
 var s2h = require('snabbdom-to-html')
 
 //*
 var outer = fs.readFileSync(__dirname + '/public/mainsimple.html', 'utf-8')
-var template = fs.readFileSync('examples/public/template.html', 'utf-8')
+var template = fs.readFileSync(__dirname + '/public/template.html', 'utf-8')
 //var simple = fs.readFileSync('examples/public/simplebutton.html', 'utf-8')
 var tmpl = function (state) {
   var numArr = ['one', 'two', 'three', 'four'];
@@ -15,7 +14,7 @@ var tmpl = function (state) {
     '#mapme': {_map: {'li': numArr}},
     'button': {type: 'button', onclick: onclick, '_html': 'click me?'}
   }
-  var ret = shaved([outer, template], content)
+  var ret = st([outer, template], content)
   console.log(content)
   console.log('r',ret)
   console.log('h',s2h(ret))
@@ -26,7 +25,7 @@ var outer = fs.readFileSync(__dirname + '/public/mainsimple.html', 'utf-8')
 var section = fs.readFileSync('examples/public/section.html', 'utf-8')
 var inner = fs.readFileSync('examples/public/button.html', 'utf-8')
 var tmpl = function (state) {
-  return shaved([outer, section, inner], {
+  return st([outer, section, inner], {
     '#count': state.n,
     'button': {onclick: onclick}
   })
